bf9d8a5aac8ba6729766d3396e9ae84162400646
sanjivkv <sanjiv.1819@gmail.com>
2014-04-24 10:58:51 +0530

    CacheMatrix Solution
    Tried a Solution for the CacheMatrix Assigment isnt working yet need
    further refinement.

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,37 @@
 ## functions do
 
 ## Write a short comment describing this function
+# Create Caches Matrix and use the Solve Function for the Inverse 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  setInverse <- function(solve) m <<- solve
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse
+       )
 }
 
 
 ## Write a short comment describing this function
+## The CacheSolveFunction tries to return from Cache if Inverse is already done
 
 cacheSolve <- function(x, ...) {
+  m<-x$getInverse
+  if(!is.null(m)){
+    message("Getting Cached Data")
+    return(m)
+    data<-x$get()
+    m<-solve(data,...)
+    x$setInverse(m)
+    m
+  }
         ## Return a matrix that is the inverse of 'x'
 }
